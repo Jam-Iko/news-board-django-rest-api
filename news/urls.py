e65a88c5d9f67fc2a7a5bbcce7ecef4ea82327fc
LucyIkonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2020-08-19 10:26:24 +0300

    Fix style in urls.py, add allowed hosts to settings.py

--- news/urls.py
+++ news/urls.py
@@ -5,14 +5,28 @@ from news import views
 urlpatterns = format_suffix_patterns(
     [
         path("", views.api_root),
-        path("posts/", views.PostListView.as_view(), name="post-list"),
-        path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-        path("comments/", views.CommentListView.as_view(), name="comment-list"),
+        path("posts/",
+            views.PostListView.as_view(),
+            name="post-list"
+        ),
+        path(
+            "posts/<int:pk>/",
+            views.PostDetailView.as_view(),
+            name="post-detail"
+        ),
+        path("comments/",
+            views.CommentListView.as_view(),
+            name="comment-list"
+        ),
         path(
             "comments/<int:pk>/",
             views.CommentDetailView.as_view(),
             name="comment-detail",
         ),
-        path("posts/<int:pk>/upvote/", views.UpvoteView.as_view(), name="upvote-view"),
+        path(
+            "posts/<int:pk>/upvote/",
+            views.UpvoteView.as_view(),
+            name="upvote-view"
+        ),
     ]
 )
